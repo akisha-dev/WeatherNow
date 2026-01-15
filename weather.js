5dd7e0328c2324f43c705ea81fb4b8fefa2ec210
Akanksha <priyaakanksha031@gmail.com>
2026-01-15 19:30:39 +0530

    implemented a loading animation

--- weather.js
+++ weather.js
@@ -1,13 +1,34 @@
- let currentconditionHtml ="";
+document.querySelector('.loader').style.display = 'none';
+let currentconditionHtml ="";
             async function getweather(){
                 try{
-                  let response = await fetch('https://wttr.in/London?format=j1');  
+                  let city= document.getElementById('city').value;
+                  let response = await fetch(`https://wttr.in/${city}?format=j1`); 
+                  document.querySelector('.loader').style.display = 'block';
                   let data = await response.json()
-                  console.log('feelslikec:',data.current_condition[0].FeelsLikeC);
+                  document.querySelector('.loader').style.display = 'none';
+                  currentconditionHtml =`
+                  <div class="results">
+                   <div class=situ>THE CURRENT WEATHER IN ${city} </div>
+                   <div class="time">Local Observation Time : ${data.current_condition[0].localObsDateTime}</div>
+                   <div class=tempC>Temperature(in Celsius) : ${data.current_condition[0].temp_C}</div>
+                    <div class=tempF>Temperature(in Farheniet) : ${data.current_condition[0].temp_F}</div>
+                    <div class=situation>${data.current_condition[0].weatherDesc[0].value}
+                    <div class=feelslikeC>Feels Like(C) : ${data.current_condition[0].FeelsLikeC}</div>
+                    <div class=feelslikeF>Feels Like(F) ${data.current_condition[0].FeelsLikeF}</div>
+                    <div class=humidity>Humidity : ${data.current_condition[0].humidity}</div>
+                    </div>
+                   `
 
+                    document.querySelector('.current')
+                    .innerHTML= currentconditionHtml;
+                   
                 }      
                 catch(error){
                     console.log('error',error);
+                     document.querySelector('.loader').style.display = 'none';
+                    document.querySelector('.current').innerHTML = `${error} Cant't fetch the data`;
                 }          
             }
-            getweather();
+         
+            
