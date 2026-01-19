3124b66948c985e852e6ff42d60fcaa6f8157bb7
Akanksha <priyaakanksha031@gmail.com>
2026-01-19 14:17:59 +0530

    -added a checker in case user inputs gibberish

--- weather.js
+++ weather.js
@@ -2,11 +2,15 @@ document.querySelector('.loader').style.display = 'none';
 let currentconditionHtml ="";
             async function getweather(){
                 try{
+
                   let city= document.getElementById('city').value;
                   let response = await fetch(`https://wttr.in/${city}?format=j1`); 
                   document.querySelector('.loader').style.display = 'block';
                   let data = await response.json()
+                  console.log(data);
+                
                   document.querySelector('.loader').style.display = 'none';
+                   if(data.nearest_area[0].areaName[0].value!= 'New Found Out'){
                   currentconditionHtml =`
                   <div class="results">
                    <div class=situ>THE CURRENT WEATHER IN ${city} </div>
@@ -21,13 +25,19 @@ let currentconditionHtml ="";
                    `
 
                     document.querySelector('.current')
-                    .innerHTML= currentconditionHtml;
+                    .innerHTML= currentconditionHtml;}
+                    else{
+                        
+                    document.querySelector('.current')
+                    .innerHTML= `${city} not found`;
+
+                    }
                    
                 }      
                 catch(error){
                     console.log('error',error);
                      document.querySelector('.loader').style.display = 'none';
-                    document.querySelector('.current').innerHTML = `${error} Cant't fetch the data`;
+                    document.querySelector('.current').innerHTML = `${error} Please Try Again`;
                 }          
             }
          
